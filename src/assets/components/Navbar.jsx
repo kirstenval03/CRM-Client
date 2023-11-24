27cf768afb80e45161c439b36cc8c623aa042fb4
kirstenval03 <kirs0307@gmail.com>
2023-11-23 23:08:41 -0600

    auth context

---
+++ src/assets/components/Navbar.jsx
@@ -0,0 +1,4 @@
+import { Link } from "react-router-dom";
+import { useContext } from "react";
+import { AuthContext } from "../context/auth.context";
+import { isStaffUser } from "../utils/authUtils";
---
+++ src/assets/context/auth.context.jsx
@@ -0,0 +1,73 @@
+import { useState, useEffect, createContext } from "react";
+import { useNavigate } from "react-router-dom";
+
+import { get } from "../services/authService";
+
+const AuthContext = createContext();
+
+function AuthProvider({ children }) {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
+  const [user, setUser] = useState(null);
+
+  const navigate = useNavigate()
+
+  const storeToken = (token) => {      
+    localStorage.setItem('authToken', token);
+  }
+
+  const removeToken = () => {                    
+    localStorage.removeItem("authToken");
+  }
+
+  const authenticateUser = () => {
+    const storedToken = localStorage.getItem('authToken');
+
+    if (storedToken) {
+
+    get('/auth/verify')
+      .then((response) => {
+
+        const user = response.data;
+      
+        setIsLoggedIn(true);
+        setIsLoading(false);
+        setUser(user);        
+      })
+      .catch((error) => {
+
+        removeToken();       
+        setIsLoggedIn(false);
+        setIsLoading(false);
+        setUser(null);        
+      });      
+    } else {
+
+        setIsLoggedIn(false);
+        setIsLoading(false);
+        setUser(null);      
+    }   
+  }
+  
+  const logOutUser = () => {      
+    removeToken();
+  
+    authenticateUser();
+    navigate('/')
+  }  
+ 
+  
+  useEffect(() => {   
+    authenticateUser()              
+
+  }, []);
+  
+
+  return (
+    <AuthContext.Provider value={{ isLoggedIn, isLoading, user, storeToken, authenticateUser, logOutUser }}>
+      {children}
+    </AuthContext.Provider>
+  )
+}
+
+export { AuthProvider, AuthContext };
