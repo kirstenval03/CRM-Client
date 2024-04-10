cc175c2d2e6d0c863c141dba3b39ac1114018fcd
kirstenval03 <kirs0307@gmail.com>
2024-04-10 12:16:09 -0600

    Slow drag fixed

--- src/services/SERVER_URL.js
+++ src/services/SERVER_URL.js
@@ -1,3 +1,3 @@
-//export const SERVER_URL = "http://localhost:4000";
+ //export const SERVER_URL = "http://localhost:4000";
 
- export const SERVER_URL = "https://e3-crm.adaptable.app";
+export const SERVER_URL = "https://e3-crm.adaptable.app";
